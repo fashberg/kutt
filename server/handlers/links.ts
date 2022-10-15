d563405dbf23cc099740df270705afc5f324d0cf
Folke Ashberg <folke@ashberg.de>
2022-10-15 22:54:25 +0200

    linting

--- server/handlers/links.ts
+++ server/handlers/links.ts
@@ -155,7 +155,7 @@ export const edit: Handler = async (req, res) => {
       ...(address && { address }),
       ...(description && { description }),
       ...(target && { target }),
-      ...(expire_in),
+      ...(expire_in && expire_in),
       ...(password && { password })
     }
   );
